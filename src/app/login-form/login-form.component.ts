390750546d04e5da95262f525f60e6dbac082b58
Jeremy Gooch <jeremy.gooch@gmail.com>
2016-08-16 22:37:33 -0500

    Adding basic validation to login form.

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators } from '@angular/common';
+import { Validators, NgClass } from '@angular/common';
 import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 
@@ -7,24 +7,37 @@ import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormControl, FormBuilder } from '@
     moduleId: module.id,
     selector: 'login-form',
     templateUrl: 'login-form.component.html',
-    directives: [ REACTIVE_FORM_DIRECTIVES ],
+    directives: [ REACTIVE_FORM_DIRECTIVES, NgClass ],
     styleUrls: ['login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
     public loginForm: FormGroup;
     public submitted: boolean;
+    public events: any[] = [];
 
     constructor(private _fb: FormBuilder) { }
 
     ngOnInit() {
         this.loginForm = this._fb.group({
             email: ['', [<any>Validators.required]],
-            password: ['', [<any>Validators.required]]
+            password: ['', [<any>Validators.required, <any>Validators.minLength(6)]]
         });
     }
 
-    doLogin(event) {
-        console.log(this.loginForm.value);
+    subcribeToFormChanges() {
+        const loginFormStatusChanges$ = this.loginForm.statusChanges;
+        const loginFormValueChanges$ = this.loginForm.valueChanges;
+
+        loginFormStatusChanges$.subscribe(x => this.events.push({ event: 'STATUS_CHANGED', object: x }));
+        loginFormValueChanges$.subscribe(x => this.events.push({ event: 'VALUE_CHANGED', object: x }));
+    }
+
+    doLogin(event, model:this, isValid:boolean) {
         event.preventDefault();
+
+        this.submitted = true;
+        if (isValid) {
+            console.log('going to validate creds');
+        }
     }
 }
